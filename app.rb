435532a39d8a78b6deb021baebc2a8188173098f
yukimura1227 <takamura1227@gmail.com>
2020-01-20 05:50:21 +0900

    fix: lint

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ end
 
 def client
   @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    config.channel_token = ENV['LINE_CHANNEL_TOKEN']
   }
 end
 
@@ -17,27 +17,26 @@ post '/callback' do
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
+    error 400 do
+      'Bad Request'
+    end
   end
 
   events = client.parse_events_from(body)
-  events.each { |event|
+  events.each do |event|
     case event
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
+        message = { type: 'text', text: event.message['text'] }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
+        tf = Tempfile.open('content')
         tf.write(response.body)
       end
     end
-  }
+  end
 
-  "OK"
+  'OK'
 end

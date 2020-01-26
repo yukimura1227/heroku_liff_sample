45262d52c406f704c75b4ecace76124d4ed18b43
yukimura1227 <takamura1227@gmail.com>
2020-01-26 11:23:37 +0900

    feat: bundle exec rake db:create_migration NAME=create_notes

---
+++ db/migrate/20200126022038_create_notes.rb
@@ -0,0 +1,15 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t| 
+      t.integer :icon_type
+      t.string :name
+      t.string :key1
+      t.string :key2
+      t.string :favorite_food1
+      t.string :favorite_food2
+      t.string :favorite_food3
+      t.string :favorite_food4
+      t.string :favorite_food5
+    end
+  end
+end
